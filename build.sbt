f0f38f98dc706ae20f59cb0893498942d918a8e1
Kálmán Ferenc <kalman.ferenc@akribis.hu>
2018-03-27 19:08:15 +0200

    Separate test and integration tests

--- build.sbt
+++ build.sbt
@@ -19,13 +19,15 @@ lazy val springmvc = (project in file("modules/spring-mvc"))
   .dependsOn(ickenham)
 
 lazy val root = (project in file("."))
+  .configs(IntegrationTest)
   .settings(
+    Defaults.itSettings,
     libraryDependencies ++= Seq(
-      "org.scalatest" %% "scalatest" % "3.0.4" % "test",
-      "com.storm-enroute" %% "scalameter" % "0.9" % "test",
-      "com.github.spullara.mustache.java" % "compiler" % "0.9.4" % "test",
-      "me.lessis" %% "fixie-grips-json4s" % "0.1.0" % "test",
-      "org.json4s" %% "json4s-jackson" % "3.5.3" % "test"
+      "org.scalatest" %% "scalatest" % "3.0.4" % "it,test",
+      "com.storm-enroute" %% "scalameter" % "0.9" % "it,test",
+      "com.github.spullara.mustache.java" % "compiler" % "0.9.4" % "it,test",
+      "me.lessis" %% "fixie-grips-json4s" % "0.1.0" % "it,test",
+      "org.json4s" %% "json4s-jackson" % "3.5.3" % "it,test"
     ),
     testFrameworks += new TestFramework("org.scalameter.ScalaMeterFramework")
   )
---
+++ src/it/scala/com/github/enpassant/ickenham/DiscussionData.scala
@@ -0,0 +1,97 @@
+package com.github.enpassant.ickenham
+
+import com.github.enpassant.ickenham.adapter.Json4sAdapter
+import com.github.enpassant.ickenham.adapter.PlainAdapter
+import com.github.enpassant.ickenham.adapter.JavaAdapter
+
+import collection.JavaConverters._
+
+import org.json4s.JsonAST._
+import org.scalatest._
+
+object DiscussonData {
+  val adapter = new Json4sAdapter()
+
+  val discussion = List(JObject(
+    "_id" -> JString("5"),
+    "escape" -> JString("5 < 6"),
+    "comments" -> JArray(List(
+      JObject(
+        "commentId" -> JString("7"),
+        "userName" -> JString("John"),
+        "content" -> JString("<h1>Test comment 1</h1>"),
+        "comments" -> JArray(List(
+          JObject(
+            "commentId" -> JString("8"),
+            "userName" -> JString("Susan"),
+            "content" -> JString("<h2>Reply</h2>"),
+            "comments" -> JNothing
+          )
+        ))
+      ),
+      JObject(
+        "commentId" -> JString("9"),
+        "userName" -> JString("George"),
+        "content" -> JString("<h1>Test comment 2</h1>"),
+        "comments" -> JNothing
+      )
+    ))
+  ))
+
+  val adapterPlain = new PlainAdapter()
+
+  val discussionPlain = List(Map(
+    "_id" -> 5,
+    "escape" -> "5 < 6",
+    "comments" -> List(
+      Map(
+        "commentId" -> "7",
+        "userName" -> "John",
+        "content" -> "<h1>Test comment 1</h1>",
+        "comments" -> List(
+          Map(
+            "commentId" -> "8",
+            "userName" -> "Susan",
+            "content" -> "<h2>Reply</h2>"
+          )
+        )
+      ),
+      Map(
+        "commentId" -> "9",
+        "userName" -> "George",
+        "content" -> "<h1>Test comment 2</h1>"
+      )
+    )
+  ))
+
+  val adapterJava = new JavaAdapter()
+
+  val discussionJava = List(Map(
+    "_id" -> 5,
+    "escape" -> "5 < 6",
+    "comments" -> List(
+      Map(
+        "commentId" -> "7",
+        "userName" -> "John",
+        "content" -> "<h1>Test comment 1</h1>",
+        "comments" -> List(
+          Map(
+            "commentId" -> "8",
+            "userName" -> "Susan",
+            "content" -> "<h2>Reply</h2>"
+          ).asJava
+        ).asJava
+      ).asJava,
+      Map(
+        "commentId" -> "9",
+        "userName" -> "George",
+        "content" -> "<h1>Test comment 2</h1>"
+      ).asJava
+    ).asJava
+  ).asJava)
+
+  class Person(val name: String, val age: Int) {
+    def getName = name
+    def getAge = age
+  }
+}
